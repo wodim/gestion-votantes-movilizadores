00f14355ece3c7e2c707b12f36c6ca685162687d
Diego Olmi <diegolmi@hotmail.com>
2023-03-06 19:26:19 -0300

    configuracion build y axiosInstance

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_URI,
+    baseURL: 'https://backgestionvotantes.com.ar/api',
    // timeout: 1000,
     headers: {
         'Content-Type': 'application/json',
