24faddb0bedef023765730442aed71e01ae8fec5
Diego Olmi <diegolmi@hotmail.com>
2023-03-06 20:43:27 -0300

    getusers

--- src/pages/mobilizador/MobilizerList.jsx
+++ src/pages/mobilizador/MobilizerList.jsx
@@ -1,61 +1,63 @@
+import React, { useState, useEffect } from "react";
 import { Card, CardBody } from "reactstrap";
 import MobilizerCamera from "./MobilizerCamera";
+import { axiosInstance } from "../../config/axiosInstance";
+import jwtDecode from "jwt-decode";
 
 const MobilizerList = () => {
+  const [userData, setUserData] = useState([]);
 
-  const data = [
-    {
-      name: "Franco",
-      lastName: "Olmi",
-      documento: 4219378,
-      circuito: 4,
-      id: 1,
-    },
-    {
-      name: "Juan",
-      lastName: "Manzur",
-      documento: 4219378,
-      circuito: 4,
-      id: 2,
-    },
-    {
-      name: "Marcos",
-      lastName: "Herrera",
-      documento: 4219378,
-      circuito: 4,
-      id: 3,
-    },
-    {
-      name: "Luis",
-      lastName: "Beltran",
-      documento: 4219378,
-      circuito: 4,
-      id: 4,
-    },
-    {
-      name: "Jorge",
-      lastName: "Olmi",
-      documento: 4219378,
-      circuito: 4,
-      id: 5,
-    },
-  ];
+  const usuarios = async (page) => {
+    const token = localStorage.getItem("token");
+    const decodedToken = token ? jwtDecode(token) : null;
+    const user = decodedToken?.user_id; //localStorage.getItem('token');
+
+    await axiosInstance
+      .get(`users/votantes/${user}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((response) => {
+        setUserData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    usuarios();
+  }, []);
+
+  console.log(userData, "users");
+
+  // const data = [
+  //   {
+  //     name: users.nombres,
+  //     lastName: users.apellido,
+  //     documento: users.documento,
+  //     circuito: users.circuito,
+  //     id: users.documento,
+  //   },
+  // ];
 
   return (
     <div className="mt-5">
       <h3 className="container">Movilizadores</h3>
-      {data.map((item) => {
+
+      {userData.map((item) => {
         return (
           <Card className="cardMobilizer" key={item.id}>
             <CardBody>
               <p>
-                Nombre: {item.name} {item.lastName}
+                Nombre: {item.nombres} {item.apellido}
               </p>
               <p>Circuito: {item.circuito}</p>
               <p>Documento: {item.documento}</p>
             </CardBody>
             <div></div>
-            <MobilizerCamera/>
+            <MobilizerCamera />
           </Card>
         );
       })}
