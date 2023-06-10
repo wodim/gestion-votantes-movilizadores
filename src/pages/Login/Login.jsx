e26c9e41052b39eead5637ab78f03b8e00a8ff3c
Diego Olmi <diegolmi@hotmail.com>
2023-06-10 19:50:52 -0300

    saque login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -14,8 +14,8 @@ const LoginForm = () => {
 
   return (
     <div className="login_container">
-      {/* ERROR 404 */}
-      <div className="formLogin">
+      ERROR 404
+      {/* <div className="formLogin">
         <div className="login_title">
           <h4>Iniciar Sesión</h4>
         </div>
@@ -80,7 +80,7 @@ const LoginForm = () => {
             </Form>
           )}
         </Formik>
-      </div>
+      </div> */}
     </div>
   );
 };
